b98f166236efdd73f5089874e8ae9fab0765b832
Jeroen Lemmen <jeroenlemmen@msn.com>
2013-11-28 11:56:10 +0100

    departure toegevoegd
    alweer eerste commit is verdwenen?

--- src/controller/Server.java
+++ src/controller/Server.java
@@ -11,65 +11,107 @@ package controller;
 
 import java.io.*;
 import java.net.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class Server {
     
-    ServerSocket providerSocket;
-    Socket connection = null;
+    List<HashMap<String, String>> containerList = new ArrayList<HashMap<String, String>>();
+    
+    ServerSocket serverSocket;
+    Socket socket = null;
     ObjectOutputStream out;
     ObjectInputStream in;
     String message;
 
-    Server() 
-    {        
+    Server(List<HashMap<String, String>> containerList) 
+    { 
+        this.containerList = containerList;
     }
     
-   public void run() {
+//   public void run() {
+//        try {
+//            //creating a server socket
+//            providerSocket = new ServerSocket(2004, 10);
+//            
+//            // Wait for socket
+//            System.out.println("Waiting for socket");
+//            socket = providerSocket.accept();
+//            System.out.println("Connection received from " + socket.getInetAddress().getHostName());
+//            
+//            //get Input and Output streams
+//            out = new ObjectOutputStream(socket.getOutputStream());
+//            out.flush();
+//            in = new ObjectInputStream(socket.getInputStream());
+//            
+//            
+//            sendMessage(containerList.get(0).toString());
+//            
+//
+//            //The two parts communicate via the input and output streams
+//            try 
+//            {
+//                message = (String) in.readObject();
+//                System.out.println("ONTVANGEN>" + message);
+//
+//            } catch (ClassNotFoundException classnot) 
+//            {
+//                System.err.println("Data received in unknown format");
+//            }
+//
+//
+//        } 
+//        catch (IOException ioException) 
+//        {
+//            ioException.printStackTrace();
+//        } 
+//        finally 
+//        {
+//            //4: Closing socket
+//            try 
+//            {
+//                in.close();
+//                out.close();
+//                providerSocket.close();
+//            } catch (IOException ioException) {
+//                ioException.printStackTrace();
+//            }
+//        }
+//    }		
+    
+    public void run()
+    {
         try {
-            //creating a server socket
-            providerSocket = new ServerSocket(2004, 10);
+            //create a server socket
+            serverSocket = new ServerSocket(2004, 10);
             
-            // Wait for connection
-            System.out.println("Waiting for connection");
-            connection = providerSocket.accept();
-            System.out.println("Connection received from " + connection.getInetAddress().getHostName());
+            System.out.println("Waiting for socket");
+            socket = serverSocket.accept();
+            System.out.println("Connection received from " + socket.getInetAddress().getHostName());
             
-            //get Input and Output streams
-            out = new ObjectOutputStream(connection.getOutputStream());
-            out.flush();
-            in = new ObjectInputStream(connection.getInputStream());
-            sendMessage("Connectie is gelukt");
-
-            //The two parts communicate via the input and output streams
-            try 
-            {
-                message = (String) in.readObject();
-                System.out.println("ONTVANGEN>" + message);
-
-            } catch (ClassNotFoundException classnot) 
-            {
-                System.err.println("Data received in unknown format");
-            }
-
-
-        } 
+            //get Input and Output streams.
+            out = new ObjectOutputStream(socket.getOutputStream());
+            in = new ObjectInputStream(socket.getInputStream());
+            
+        }
         catch (IOException ioException) 
         {
             ioException.printStackTrace();
-        } 
-        finally 
-        {
-            //4: Closing connection
-            try 
-            {
-                in.close();
-                out.close();
-                providerSocket.close();
-            } catch (IOException ioException) {
-                ioException.printStackTrace();
-            }
         }
-    }		
+//         finally 
+//        {
+//            //4: Closing socket
+//            try 
+//            {
+//                in.close();
+//                out.close();
+//                serverSocket.close();
+//            } catch (IOException ioException) {
+//                ioException.printStackTrace();
+//            }
+//        }
+    }
    
     void sendMessage(String msg) 
     {
@@ -79,7 +121,9 @@ public class Server {
         } catch (IOException ioException) {
             ioException.printStackTrace();
         }
-    }  
+    }
+    
+    
 }
 
 
